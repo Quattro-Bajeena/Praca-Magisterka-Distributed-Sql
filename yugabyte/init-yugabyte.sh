ce5f59d0f43a765039c4f2a9caa7aff065152563
Mateusz Oleszek <mateusz.oleszek@student.put.poznan.pl>
2025-08-17 18:50:17 +0200

    yugabyte

---
+++ yugabyte/init-yugabyte.sh
@@ -0,0 +1,23 @@
+minikube start --memory=8192 --cpus=4 --disk-size=40g --vm-driver=docker
+kubectl create namespace yb-demo
+helm repo add yugabytedb https://charts.yugabyte.com
+helm repo update
+helm search repo yugabytedb/yugabyte --version 2.25.2
+helm install yb-demo yugabytedb/yugabyte \
+--version 2.25.2 \
+--set resource.master.requests.cpu=0.5,resource.master.requests.memory=0.5Gi,\
+resource.tserver.requests.cpu=0.5,resource.tserver.requests.memory=0.5Gi,\
+replicas.master=1,replicas.tserver=1,enableLoadBalancer=False --namespace yb-demo
+kubectl --namespace yb-demo get pods
+kubectl --namespace yb-demo get services
+kubectl --namespace yb-demo port-forward svc/yb-masters 7000:7000 --address=0.0.0.0 > pf-yugabyte.log &
+kubectl --namespace yb-demo exec -it yb-tserver-0 -- sh -c "cd /home/yugabyte && ysqlsh -h yb-tserver-0 --echo-queries"
+
+
+
+
+curl -O https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/cloud/kubernetes/yugabyte-statefulset-rf-1.yaml
+kubectl  apply --namespace='yb-demo' -f ./yugabyte-statefulset-rf-1.yaml
+
+
+curl  https://raw.githubusercontent.com/yugabyte/yugabyte-db/master/cloud/kubernetes/yugabyte-statefulset-rf-1.yaml | sed 's/image: yugabytedb\/yugabyte\:latest/image: yugabytedb\/yugabyte:2025.1.0.1-b3/g' | kubectl  apply --namespace='yb-demo' -f -
---
+++ yugabyte/retail-analytics.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE yb_demo;
+GRANT ALL ON DATABASE yb_demo to yugabyte;
+\c yb_demo
+\i share/schema.sql;
+\i share/products.sql;
+\i share/users.sql;
+\i share/orders.sql;
+\i share/reviews.sql;
+\d products;
+SELECT count(*) FROM products;
+SELECT id, title, category, price, rating
+          FROM products
+          LIMIT 5;
+SELECT id, title, category, price, rating
+          FROM products
+          LIMIT 3 OFFSET 5;
+
+SELECT users.id, users.name, users.email, orders.id, orders.total
+          FROM orders INNER JOIN users ON orders.user_id=users.id
+          LIMIT 10;
+
+SELECT id, category, price, quantity FROM products WHERE id=2;
+
+
+BEGIN TRANSACTION;
+/* First insert a new order into the orders table. */
+INSERT INTO orders
+  (id, created_at, user_id, product_id, discount, quantity, subtotal, tax, total)
+VALUES (
+  (SELECT max(id)+1 FROM orders)                 /* id */,
+  now()                                          /* created_at */,
+  1                                              /* user_id */,
+  2                                              /* product_id */,
+  0                                              /* discount */,
+  10                                             /* quantity */,
+  (10 * (SELECT price FROM products WHERE id=2)) /* subtotal */,
+  0                                              /* tax */,
+  (10 * (SELECT price FROM products WHERE id=2)) /* total */
+) RETURNING id;
+
+/* Next decrement the total quantity from the products table. */
+UPDATE products SET quantity = quantity - 10 WHERE id = 2;
+
+COMMIT;
+
+
+
+SELECT DISTINCT(source) FROM users;
+SELECT MIN(price), MAX(price), AVG(price) FROM products;
+
+
+SELECT source, count(*) AS num_user_signups
+          FROM users
+          GROUP BY source
+          ORDER BY num_user_signups DESC;
+
+SELECT source, ROUND(SUM(orders.total)) AS total_sales
+          FROM users LEFT JOIN orders ON users.id=orders.user_id
+          GROUP BY source
+          ORDER BY total_sales DESC;
+
+
+CREATE VIEW channel AS
+            (SELECT source, ROUND(SUM(orders.total)) AS total_sales
+             FROM users LEFT JOIN orders ON users.id=orders.user_id
+             GROUP BY source
+             ORDER BY total_sales DESC);
+
+
+\d
+
+SELECT source,
+            total_sales * 100.0 / (SELECT SUM(total_sales) FROM channel) AS percent_sales
+          FROM channel
+          WHERE source='Facebook';
