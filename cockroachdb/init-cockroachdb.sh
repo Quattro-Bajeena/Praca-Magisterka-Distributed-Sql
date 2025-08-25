09bcd6e4299ca97ac20e128817ff708403cd9922
Mateusz Oleszek <mateusz.oleszek@student.put.poznan.pl>
2025-08-26 00:23:08 +0200

    notes

--- cockroachdb/init-cockroachdb.sh
+++ cockroachdb/init-cockroachdb.sh
@@ -1,13 +1,7 @@
 minikube start --cpus=4 --memory=8192 --disk-size=40g --driver=docker --ports=30257:30257,30080:30080
 kubectl create -f ./cockroachdb-statefulset.yaml
-sleep 30
-kubectl get pods
-kubectl get pv
+sleep 10
+# kubectl wait pod --all --for=condition=Ready --timeout=200s
 kubectl create -f ./cluster-init.yaml
 sleep 10
-kubectl get job cluster-init
-kubectl get pods
-minikube service cockroachdb-public --url
-curl $(minikube ip):30080
-# docker network connect host minikube
-# kubectl port-forward service/cockroachdb-public --address=0.0.0.0 30257:26257 30080:8080 > pf-cockroachdb-public.out &
+kubectl port-forward service/cockroachdb-public --address=0.0.0.0 30257:26257 30080:8080 > pf-cockroachdb-public.out &
---
+++ cockroachdb/notes.md
@@ -0,0 +1,4 @@
+https://www.cockroachlabs.com/docs/stable/postgresql-compatibility
+https://www.cockroachlabs.com/docs/v25.3/sql-feature-support
+https://github.com/secp256k1-sha256/postgres-compatibility-index
+https://pgscorecard.com/
