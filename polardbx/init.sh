923b7cbfaa990d065689f1e8930e2b5dc8f1e5fb
Mateusz Oleszek <mateusz.oleszek.mo@outlook.com>
2026-02-11 23:59:29 +0100

    cockraoch oceanbase

--- polardbx/init.sh
+++ polardbx/init.sh
@@ -1,7 +1,13 @@
 python3 -m venv venv
 source venv/bin/activate
+.\venv\Scripts\activate
 pip install --upgrade pip
 
 pip3 install wheel -v
 pip3 install "cython<3.0.0" pyyaml==5.4.1 --no-build-isolation -v
 pip install pxd
+
+pxd tryout
+pxd list
+pxd cleanup
+pxd delete pxc-tryout
